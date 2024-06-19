d00b21975ca97c5b6ec86c2f1319ae8a082e73ad
neerajgurung2099 <neerajgurung2099@gmail.com>
2024-06-19 12:50:59 +0545

    Update

--- src/features/auth/actions/signUpAction.ts
+++ src/features/auth/actions/signUpAction.ts
@@ -3,14 +3,12 @@ import {
   SignUpCredentialsDTO,
   signUpWithEmailAndPassword,
 } from "../api/signUp";
-import { storage } from "@/utils/storage";
 
 export const signUpAction = async ({ request }: LoaderFunctionArgs) => {
   try {
     const data: SignUpCredentialsDTO = await request.json();
     Reflect.deleteProperty(data, "confirmPassword");
     const response = await signUpWithEmailAndPassword(data);
-    storage.setToken(response.token);
     return response;
   } catch (error) {
     return null;
